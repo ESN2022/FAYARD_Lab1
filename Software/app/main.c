9a3db4092eedf4a85fb76ff70efb806bec5bf0bd
JessicaFyrd <jessica.fayard1993@laposte.net>
2023-01-03 17:25:45 +0100

    Add the possibility to chose the speed with the slide buttons (0 to 3)

--- Software/app/main.c
+++ Software/app/main.c
@@ -1,18 +1,40 @@
 #include<system.h>
 #include<altera_avalon_pio_regs.h>
+#include <stdio.h>
 
-int i = 0;
+int b, i = 0, t;
 
 int main(int argc, char *argv[])
 {
     while(1){
+        b = IORD_ALTERA_AVALON_PIO_DATA(BUTTONS_BASE);
+        switch (b)
+        {
+        case 1:
+            t = 600000;
+            break;
+        case 2:
+            t = 400000;
+            break;
+        case 4:
+            t = 100000;
+            break;
+        case 8:
+            t = 50000;
+            break;
+        default:
+            t = 200000;
+            break;
+        }
+
+
         for (i=0;i<7;i++){
            IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE,1<<i);
-           usleep(100000);
+           usleep(t);
         }
         for (i=7;i>0;i--){
            IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE,1<<i);
-           usleep(100000);
+           usleep(t);
         }
     }
 
